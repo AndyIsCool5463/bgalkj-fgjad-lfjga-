c6edc9108ea8727fb3d775a3e100e5e7a166eb0a
andyl5463-school <www.andylyek2006@gmail.com>
2019-09-25 03:57:06 -0700

    testing

---
+++ build/precache-manifest.13de675f40b6fea603cccacb0be8eeb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c3328a8b2d10f3421709029988c287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c484038d741e33309c96",
+    "url": "/static/css/2.76624747.chunk.css"
+  },
+  {
+    "revision": "f25c69e27ea6fb75763f",
+    "url": "/static/css/main.1e6c92ab.chunk.css"
+  },
+  {
+    "revision": "c484038d741e33309c96",
+    "url": "/static/js/2.0496e069.chunk.js"
+  },
+  {
+    "revision": "f25c69e27ea6fb75763f",
+    "url": "/static/js/main.eaa2a9d2.chunk.js"
+  },
+  {
+    "revision": "b3d0ab8069d62a24addb",
+    "url": "/static/js/runtime-main.a9f9040a.js"
+  }
+]);
